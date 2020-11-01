6de7247b4ec7e2920a6683b9a84c39da4f97b013
alexhkz <alexwebcode@gmail.com>
2020-11-01 16:17:29 +0300

    add-posts

--- src/components/post-add-form/post-add-form.js
+++ src/components/post-add-form/post-add-form.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const PostAddForm = () => {
+const PostAddForm = ({onAdd}) => {
 	return (
-		<form className="bottom-panel d-flex">
+		<div className="bottom-panel d-flex">
 			<input
 				type="text"
 				placeholder="О чм вы думаете сейчас?"
@@ -10,9 +10,11 @@ const PostAddForm = () => {
 			/>
 			<button
 				type="submit"
-				className="btn btn-outline-secondary">
-				Добавить</button>
-		</form>
+				className="btn btn-outline-secondary"
+				onClick={() => onAdd('Hello')}>
+				Добавить
+				</button>
+		</div>
 	)
 }
 
